778ff982e39d4b7b346085905f8fd5bde8deddc1
enthu <enthu@UN1CORN>
2023-10-07 23:48:14 +0700

    Добавьте файлы проекта.

---
+++ library.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+// Структуры
+struct Company {
+    char* name;
+    char* found_date;
+    char* address;
+};
+
+struct Weapon {
+    char* name;
+    Company company;
+    int rel_year;
+};
+
+struct Soldier {
+    char* name;
+    char* draft_date;
+    char* address;
+};
+
+struct Control {
+    char* operation;
+    char* date;
+    Weapon weapon;
+    Soldier human;
+};
+
+struct Armory {
+    Weapon* weapons;
+    Soldier* soldiers;
+    Control* operations;
+    char* military;
+};
+
+// Вспомогательные функции
+
+char vvod(char min, char max) { // автоматизировал проверку на допустимость ввода, ввод без нажатия ENTER
+    char x;
+    do {
+        x = _getch();
+    } while (x < min || x > max);
+    return x;
+}
